45204651f3c70e5211af51364bdf6de23ed0d0b5
Emiya-lhl <2511255571@qq.com>
2020-12-30 11:55:00 +0800

    初始化

--- src/main/java/com/lhl/datacenter/DatacenterApplication.java
+++ src/main/java/com/lhl/datacenter/DatacenterApplication.java
@@ -1,8 +1,10 @@
 package com.lhl.datacenter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.ServletComponentScan;
+@MapperScan(basePackages = "com.lhl.datacenter.dao")
 @SpringBootApplication
 public class DatacenterApplication {
 
